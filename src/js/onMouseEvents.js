eb70c628be9931928305436b560a3883430311c2
Bacisor <Bacisorclaudiu@yahoo.ca>
2018-05-18 17:26:22 -0400

    added top clothing cathegories on mouseover

---
+++ src/js/onMouseEvents.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){
+   
+    // New 
+        $("#newCathegory").mouseover(function(){
+            $("#newCathegoryDiv").css("display","block");
+            $("#newCathegoryDiv").css("color","white");
+            $(".cathegorySelection ").css("height", "40px");
+            $(".cathegorySelection ").css("background-color", "black");
+            
+        });
+        $("#newCathegory").mouseout(function(){
+            $("#newCathegoryDiv").css("display", "none");
+            $("#newCathegoryDiv").css("color","white");
+            $(".cathegorySelection ").css("height", "0px");
+            $(".cathegorySelection ").css("background-color", "white");
+        });
+    
+        
+    // Womens 
+    $("#womensCathegory").mouseover(function(){
+        $("#womensCathegoryDiv").css("display","block");
+        $("#womensCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "40px");
+        $(".cathegorySelection ").css("background-color", "black");
+        
+    });
+    $("#womensCathegory").mouseout(function(){
+        $("#womensCathegoryDiv").css("display", "none");
+        $("#womensCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "0px");
+        $(".cathegorySelection ").css("background-color", "white");
+    });
+    
+    //Mens
+    $("#mensCathegory").mouseover(function(){
+        $("#mensCathegoryDiv").css("display","block");
+        $("#mensCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "40px");
+        $(".cathegorySelection ").css("background-color", "black");
+        
+    });
+    $("#mensCathegory").mouseout(function(){
+        $("#mensCathegoryDiv").css("display", "none");
+        $("#mensCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "0px");
+        $(".cathegorySelection ").css("background-color", "white");
+    });
+    
+    // Girls
+    $("#girlsCathegory").mouseover(function(){
+        $("#girlsCathegoryDiv").css("display","block");
+        $("#girlsCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "40px");
+        $(".cathegorySelection ").css("background-color", "black");
+        
+    });
+    $("#girlsCathegory").mouseout(function(){
+        $("#girlsCathegoryDiv").css("display", "none");
+        $("#girlsCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "0px");
+        $(".cathegorySelection ").css("background-color", "white");
+    });
+    
+    // Boys
+    $("#boysCathegory").mouseover(function(){
+        $("#boysCathegoryDiv").css("display","block");
+        $("#boysCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "40px");
+        $(".cathegorySelection ").css("background-color", "black");
+        
+    });
+    $("#boysCathegory").mouseout(function(){
+        $("#boysCathegoryDiv").css("display", "none");
+        $("#boysCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "0px");
+        $(".cathegorySelection ").css("background-color", "white");
+    });
+    
+    // beautyCathegory
+    $("#beautyCathegory").mouseover(function(){
+        $("#beautyCathegoryDiv").css("display","block");
+        $("#beautyCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "40px");
+        $(".cathegorySelection ").css("background-color", "black");
+        
+    });
+    $("#beautyCathegory").mouseout(function(){
+        $("#beautyCathegoryDiv").css("display", "none");
+        $("#beautyCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "0px");
+        $(".cathegorySelection ").css("background-color", "white");
+    });
+    
+    // Home
+    $("#homeCathegory").mouseover(function(){
+        $("#homeCathegoryDiv").css("display","block");
+        $("#homeCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "40px");
+        $(".cathegorySelection ").css("background-color", "black");
+        
+    });
+    $("#homeCathegory").mouseout(function(){
+        $("#homeCathegoryDiv").css("display", "none");
+        $("#homeCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "0px");
+        $(".cathegorySelection ").css("background-color", "white");
+    });
+    
+    // Sale
+    $("#saleCathegory").mouseover(function(){
+        $("#saleCathegoryDiv").css("display","block");
+        $("#saleCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "40px");
+        $(".cathegorySelection ").css("background-color", "black");
+        
+    });
+    $("#saleCathegory").mouseout(function(){
+        $("#saleCathegoryDiv").css("display", "none");
+        $("#saleCathegoryDiv").css("color","white");
+        $(".cathegorySelection ").css("height", "0px");
+        $(".cathegorySelection ").css("background-color", "white");
+    });
+    });
